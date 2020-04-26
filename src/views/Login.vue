c21c61999daf4f1e5bec4be785f3f5b7f7e0ddb0
Choi Changhwan <ccwhan6@gmail.com>
2020-04-26 23:08:32 +0900

    login api init

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,13 +9,16 @@
                     <div class="card-header bg-transparent">
                             <div class="text-center mb-3"><img src = "img/brand/unnamed2.png"></div>
                             <div class="text-muted text-center mb-3">
-                                <large>Sign in with Google</large>
+                                <p>Sign in with Google</p>
                             </div>
                             <div class="btn-wrapper text-center">
-                                <a href="#/selectType" class="btn btn-neutral btn-icon btn-block btn-size">
+                                <!-- <button data-onsuccess="onSignIn" class="btn btn-neutral btn-icon btn-block btn-size">
                                     <span class="btn-inner--icon"><img src="img/icons/common/google.svg"></span>
                                     <span class="btn-inner--text">Google</span>
-                                </a>
+                                </button> -->
+                                <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess" :onFailure="onFailure"></GoogleLogin>
+                                 <!-- <GoogleLogin :params="params" :onSuccess="onSuccess" :onFailure="onFailure">Login</GoogleLogin> -->
+                                 <GoogleLogin :params="params" :logoutButton=true>Logout</GoogleLogin>
                                 <p></p>
                             </div>
                         </div>
@@ -24,17 +27,40 @@
         </div>
 </template>
 <script>
+  import GoogleLogin from 'vue-google-login';
+
   export default {
     name: 'login',
-    data() {
-      return {
-        model: {
-          email: '',
-          password: ''
+     data() {
+            return {
+                // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+                params: {
+                    client_id: "656386263034-8fq6oqa3ei48nt0t1mjrk2ri4ribrmbk.apps.googleusercontent.com"
+                },
+                // only needed if you want to render the button with the google ui
+                renderParams: {
+                    width: 375,
+                    height: 50,
+                    longtitle: true
+                }
+            }
+        },
+        components: {
+            GoogleLogin
+        },
+        methods: {
+        onSuccess(googleUser) {
+            console.log(googleUser);
+ 
+            // This only gets the user information: id, name, imageUrl and email
+            console.log(googleUser.getBasicProfile());
+        },
+        onFailure(error) {
+          console.log(error);
         }
-      }
     }
   }
+
 </script>
 <style>
 </style>
