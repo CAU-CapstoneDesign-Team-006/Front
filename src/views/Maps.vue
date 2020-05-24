7a71fa02303b1a7a7b0c27a96609c606355f3475
Choi Changhwan <ccwhan6@naver.com>
2020-05-24 23:24:17 +0900

    add marker filtering

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -2,7 +2,11 @@
     <div>
 
         <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-            <button></button>
+            <base-button type="primary" @click="markerFiltering('여가')">여가</base-button>
+            <base-button type="primary" @click="markerFiltering('디저트')">디저트</base-button>
+            <base-button type="primary" @click="markerFiltering('음식점')">음식점</base-button>
+            <base-button type="primary" @click="markerFiltering('카페')">카페</base-button>
+            <base-button type="primary" @click="markerFiltering('all')">모두보기</base-button>
         </base-header>
         <div class="container-fluid mt--7">
             <div class="row">
@@ -47,7 +51,10 @@
                 modals: {
                     modal0 : false
                 },
-                exp : String
+                exp : String,
+                kakaomap : Object,
+                markers: [],
+                filter: "all"
             }
         },
         methods : {
@@ -59,6 +66,7 @@
                     level: 3 
                 }; 
                 var map = new kakao.maps.Map(container, options); 
+                vm.kakaomap = map;
                 kakao.maps.event.addListener(map, 'idle', function() {
                     vm.fetchMarker(map);
                 });
@@ -86,8 +94,12 @@
                 params.append('longitude1', neLng);
                 axios
                     .post('http://ec2-13-125-55-59.ap-northeast-2.compute.amazonaws.com:3000/find', params) 
-                    .then(res => { 
+                    .then(res => {
                         vm.mks = res.data
+                        for (let i in vm.markers){
+                            vm.markers[i].setMap(null);
+                        }
+                        vm.markers = [];
                         for (var i in vm.mks){
                             let marker = new kakao.maps.Marker({
                                 map: map,
@@ -96,17 +108,37 @@
                             });
                             kakao.maps.event.addListener(marker, 'click', function() {
                                 let temp = marker.getZIndex();
-                                vm.exp = vm.mks[temp].name + " : " + vm.mks[temp].information 
+                                vm.exp = vm.mks[temp].name + "(" + vm.mks[temp].category + ")" + " : " + vm.mks[temp].information 
                                 vm.modals.modal0 = true;
                             });
+                            vm.markers.push(marker);
                         }
+                        vm.markerFiltering(vm.filter);
                     })
                     .catch(ex =>{
                         console.log('why')
                     })
             },
-            markerClick(zidx){
-
+            markerFiltering(filter) {
+                var vm = this;
+                let map = vm.kakaomap;
+                vm.filter = filter;
+                if (vm.filter == "all"){
+                    for (let i in vm.markers){
+                        vm.markers[i].setMap(map);
+                    }
+                }
+                else {
+                    for (let i in vm.markers){
+                        let temp = vm.markers[i].getZIndex();
+                        if (vm.mks[temp].category == vm.filter){
+                            vm.markers[i].setMap(map)
+                        }
+                        else {
+                            vm.markers[i].setMap(null);
+                        }
+                    }
+                }
             }
         }
     }
