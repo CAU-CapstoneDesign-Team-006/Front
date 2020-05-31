bac4fcafe46ba276d3dc6f6af3cadf50a2480ca5
Choi Changhwan <ccwhan6@naver.com>
2020-05-31 22:33:08 +0900

    마커이미지, 카테고리분류변경 및 현재위치로 설정

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -2,11 +2,18 @@
     <div>
 
         <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
-            <base-button type="primary" @click="markerFiltering('여가')">여가</base-button>
-            <base-button type="primary" @click="markerFiltering('디저트')">디저트</base-button>
-            <base-button type="primary" @click="markerFiltering('음식점')">음식점</base-button>
-            <base-button type="primary" @click="markerFiltering('카페')">카페</base-button>
-            <base-button type="primary" @click="markerFiltering('all')">모두보기</base-button>
+            <base-dropdown>
+                <base-button slot="title" type="secondary" class="dropdown-toggle">
+                    {{filter}}
+                </base-button>
+                <a class="dropdown-item" href="#" @click="markerFiltering('여가')">여가</a>
+                <a class="dropdown-item" href="#" @click="markerFiltering('도넛')">도넛</a>
+                <a class="dropdown-item" href="#" @click="markerFiltering('커피')">커피</a>
+                <a class="dropdown-item" href="#" @click="markerFiltering('아이스크림')">아이스크림</a>
+                <a class="dropdown-item" href="#" @click="markerFiltering('음식점')">음식점</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" @click="markerFiltering('all')">모두보기</a>
+            </base-dropdown>
         </base-header>
         <div class="container-fluid mt--7">
             <div class="row">
@@ -39,8 +46,7 @@
     export default {
         mounted() {
             window.kakao && window.kakao.maps ? this.initMap() : this.addScript();
-            // console.log(this.$route.params)
-            
+            // console.log(this.$route.params)    
         },
         components: {
             modal
@@ -54,19 +60,39 @@
                 exp : String,
                 kakaomap : Object,
                 markers: [],
-                filter: "all"
+                filter: "all",
+                category: {
+                    '도넛' : 'img/icons/iconmonstr-candy-9-64.png',
+                    '커피' : 'img/icons/iconmonstr-coffee-1-64.png',
+                    '아이스크림' : 'img/icons/iconmonstr-candy-25-64.png',
+                    '음식점' : 'img/icons/iconmonstr-eat-2-64.png',
+                    '여가' : 'img/icons/iconmonstr-video-8-64.png',
+                },
+                lat : 37.564214,
+                lon : 127.001699
             }
         },
         methods : {
             initMap() {
                 var vm = this;
-                var container = document.getElementById('map-canvas'); 
+                var container = document.getElementById('map-canvas');
+                
                 var options = { 
-                    center: new kakao.maps.LatLng(37.564214, 127.001699), 
+                    center: new kakao.maps.LatLng(vm.lat, vm.lon), 
                     level: 3 
                 }; 
                 var map = new kakao.maps.Map(container, options); 
                 vm.kakaomap = map;
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        vm.lat = position.coords.latitude // 위도
+                        vm.lon = position.coords.longitude; // 경도
+                        var locPosition = new kakao.maps.LatLng(vm.lat, vm.lon)
+                        map.setCenter(locPosition);
+                    });
+                } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+                    console.log("현재위치 조회 불가")
+                }
                 kakao.maps.event.addListener(map, 'idle', function() {
                     vm.fetchMarker(map);
                 });
@@ -101,10 +127,15 @@
                         }
                         vm.markers = [];
                         for (var i in vm.mks){
+                            var imageSrc = vm.category[vm.mks[i].category], // 마커이미지의 주소입니다    
+                            imageSize = new kakao.maps.Size(26, 26), // 마커이미지의 크기입니다
+                            imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
                             let marker = new kakao.maps.Marker({
                                 map: map,
                                 position: new kakao.maps.LatLng(vm.mks[i].latitude, vm.mks[i].longitude),
-                                zIndex: i
+                                zIndex: i,
+                                image: markerImage
                             });
                             kakao.maps.event.addListener(marker, 'click', function() {
                                 let temp = marker.getZIndex();
