df39c34f4dcd6e9e1b288f076a8f62f6b2d02899
Choi Changhwan <ccwhan6@naver.com>
2020-06-06 22:08:37 +0900

    vuex modify

--- src/store.js
+++ src/store.js
@@ -1,13 +1,20 @@
 // store.js
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    plugins: [createPersistedState()],
     state : {
         type : 1,
         name : "",
         gmail : ""
-    }
+    },
+    mutations: {
+        gmail: (state, payload) => state.gmail = payload,
+        name: (state, payload) => state.name = payload,
+        type: (state, payload) => state.type = payload
+      }
 });
